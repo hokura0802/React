33c968c579919f88113dead95ce56e8d8a67985f
hokura0802 <122342553+hokura0802@users.noreply.github.com>
2023-02-15 13:36:20 +0900

    リアクト

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Message from "./components/message";
 
 const App = () => {
@@ -7,12 +7,51 @@ const App = () => {
 	//   Fontsize:"18px"
 	//   // React上の記法として、font-sizeをFontsizeにしている。
 	// };
+	const onClickCountUp = () => {
+		setNum(Num + 1);
+	};
+
+	const [Num, setNum] = useState(0);
+	// //左側が変化する数値。右側が変化させる数値
+	// 可変の状態
+	// ⇒stateが変更されると再レンダリングされる
+	// 劇的に値が変化するときに用いる
+
+	const onClickswitchShowFlag = () => {
+		setFaseShowFlag(!FaseShowFlag);
+	};
+	const [FaseShowFlag, setFaseShowFlag] = useState(false);
+
+	useEffect(() => {
+		if (Num > 0) {
+			if (Num % 3 === 0) {
+				FaseShowFlag || setFaseShowFlag(true);
+			} else {
+				FaseShowFlag && setFaseShowFlag(false);
+			}
+		}
+	}, [Num]);
+	// ステートが最初から処理が走る
+	// UseEffectを使い、[]の中の条件で処置のタイミングを制御可能
+	// 第２引数を[]だけにすると（今回でいう[Num]のところ）最初の１回だけ実行という処理に変更できる。
+
+	// console,log(再レンダリング);
 	return (
 		<>
 			<h1 style={{ color: "red" }}>こんにちは</h1>
 			{/* <p　style={contentstyle}>お元気ですか</p> */}
 			<Message color="pink">元気です</Message>
 			<Message color="blue">元気です</Message>
+			<button onClick={onClickCountUp}>カウントアップ</button>
+			<br />
+			<button onClick={onClickswitchShowFlag}>on/off</button>
+			<button>on/off</button>
+			<p>{Num}</p>
+			{/* &&　⇒左辺がTrueなら右辺を出力する。論理演算子*/}
+			{FaseShowFlag && <p>ON</p>}
+
+			{/* ||　⇒左辺がFalseなら右辺を出力する */}
+			{/* {FaseShowFlag || <p>OFF</p>} */}
 		</>
 	);
 };
